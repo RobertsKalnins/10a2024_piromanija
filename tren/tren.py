5386ce0a797e63bd26562f89bc7b8c25f6430f9c
RobertsKalnins <robertskalnins2008@gmail.com>
2024-10-03 12:51:27 +0300

    python

--- tren/tren.py
+++ tren/tren.py
@@ -1,3 +1,9 @@
-text = str(input("Vai vajag drukāt: "))
+print("Sveiki mani dārgie piromāni")
+vards = input("Kā tevi sauc?: ")
+gadi = int(input("Cik tev ir gadi?"))
+nakamais = gadi + 1
+print((738*273)**2)
 
-print("text")
+#ir komentārs kas neparādās
+
+print(f"Tevi sauc {vards} un tev ir {gadi} gadi, un nākamgad tev būs {nakamais}")
