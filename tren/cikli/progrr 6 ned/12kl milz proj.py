cc6bcabdbdf6c2d6f4083958693338de33175901
RobertsKalnins <robertskalnins2008@gmail.com>
2024-12-19 10:43:08 +0200

    l

---
+++ tren/cikli/progrr 6 ned/12kl milz proj.py
@@ -0,0 +1,17 @@
+import os
+os.system('cls')
+
+# def hello():
+#     print("hello")
+
+# name = input("Kā tevi sauc?")
+# hello()
+# print(name)
+
+
+def hello(kam="World"):
+    print("hello", kam)
+
+
+name = input("Kā tevi sauc?")
+hello(name)
