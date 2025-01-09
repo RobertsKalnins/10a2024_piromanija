445517a5fdabeaebcbe524d42bfd8b731f7ab885
RobertsKalnins <robertskalnins2008@gmail.com>
2025-01-09 11:21:03 +0200

    Create balastvielas.py
    jupii

---
+++ tren/just uzdevumi.py/balastvielas.py
@@ -0,0 +1,33 @@
+import os
+os.system('cls')
+
+produkts = input("Ievadi ēdamo produktu:")
+
+if produkts != "Briseles kāposti":
+    print("4,4g")
+elif produkts !="Seleriju saknes":
+    print("4,2g")
+elif produkts !="Brokoļi":
+    print("3,0g")
+elif produkts !="Ziedkāposti":
+    print("2,9g")
+elif produkts !="Burkāni":
+    print("2,9g")
+elif produkts !="Sarkanās bietes":
+    print("2,5g")
+elif produkts !="Salāti":
+    print("1,8g")
+elif produkts !="Upenes":
+    print("6,8g")
+elif produkts !="Avenes":
+    print("5,0g")
+elif produkts !="Zemenes":
+    print("4,0g")
+elif produkts !="Jāņogas":
+    print("2,5g")
+elif produkts !="Āboli":
+    print("2,3g")
+elif produkts !="Apelsīni":
+    print("2,2g")
+elif produkts !="Plūmes":
+    print("1,7g")
